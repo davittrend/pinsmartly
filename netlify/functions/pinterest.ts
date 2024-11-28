8a82531f7b32752d2aa2016bc3f48264c257e5d4
davittrend <davidtrendovski@gmail.com>
2024-11-28 23:30:02 +0100

    Update pinterest.ts

--- netlify/functions/pinterest.ts
+++ netlify/functions/pinterest.ts
@@ -20,7 +20,7 @@ export const handler: Handler = async (event) => {
   }
 
   if (!clientId || !clientSecret) {
-    console.error('Missing Pinterest credentials:', { clientId, clientSecret });
+    console.error('Missing Pinterest credentials:', { clientId: !!clientId, clientSecret: !!clientSecret });
     return {
       statusCode: 500,
       headers,
@@ -35,6 +35,7 @@ export const handler: Handler = async (event) => {
       case '/oauth/url': {
         const scope = 'boards:read,pins:read,pins:write,user_accounts:read,boards:write';
         const authUrl = `https://www.pinterest.com/oauth/?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}&state=sandbox`;
+        console.log('Generated auth URL:', { clientId, redirectUri });
         return { 
           statusCode: 200, 
           headers, 
